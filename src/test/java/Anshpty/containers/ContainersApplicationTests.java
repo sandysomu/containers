bc01a3258c1976f0ec85760bb02cb69d59875455
Sandeep Singh <55424320+sandyatww@users.noreply.github.com>
2025-10-12 10:01:53 +1100

    Initial commit, vanila build

---
+++ src/test/java/Anshpty/containers/ContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package Anshpty.containers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
